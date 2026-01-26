f0cf620edd0b2a04cb8830833826a0d6dbc3d5e1
Bruce Nelson <bruce.nelson@databricks.com>
2026-01-25 19:11:47 -0800

    Add a version that deletes the old serverless SEs

--- nsp-migrate-script-new.ps1
+++ nsp-migrate-script-new.ps1
@@ -7,6 +7,10 @@
     The Azure region where the NSP will be created.
 .PARAMETER Interactive
     (optional) Boolean flag to indicate whether to run in interactive mode (prompt for each association) or unattended mode.
+    Default is $true (interactive mode).
+.PARAMETER Remove_Serverless_ServiceEndpoints
+    (optional) Boolean flag to indicate whether to remove service endpoints from Storage Accounts after associating with NSP in unattended mode.
+    Default is $false.  
 .DESCRIPTION
     This script automates the creation of a Network Security Perimeter (NSP) in Azure
     and associates Storage Accounts with Databricks VNet ACLs to the NSP in learning mode.
@@ -17,8 +21,12 @@
 
     Unattended mode:
     ./nsp-migrate-script.ps1 -Subscription_Id "<subscription id>" -Resource_Group "<resource group name>" -Azure_Region "<azure region>" -Interactive False
+
+    Remove Service endpoints in unattended mode 
+   ./nsp-migrate-script.ps1 -Subscription_Id "<subscription id>" -Resource_Group "<resource group name>" -Azure_Region "<azure region>" -Interactive False -Remove_Serverless_ServiceEndpoints True
 #>
-param( [Parameter(Mandatory)]$Subscription_Id, [Parameter(Mandatory)]$Resource_Group, [Parameter(Mandatory)]$Azure_Region, $Interactive=$true)
+
+param( [Parameter(Mandatory)]$Subscription_Id, [Parameter(Mandatory)]$Resource_Group, [Parameter(Mandatory)]$Azure_Region, $Interactive=$true, $Remove_Serverless_ServiceEndpoints=$false)
 
 # Set variables
 $subscriptionId = $Subscription_Id
@@ -36,7 +44,55 @@ Start-Transcript -Path $logPath -Append
 # interavtive or unattended mode - set to $true to approve each association, false to run unattended
 $interactive = $Interactive
 
+# Define function to remove service endpoints
+function Remove-Serverless-ServiceEndpoints {
+param( [Parameter(Mandatory)]$storageAccountName)
+# Get the subnet object to retrieve its resource ID
+$sekql = @" 
+resources
+| where type == "microsoft.storage/storageaccounts"
+| where name == '$storageAccountName'
+| where array_length(properties.networkAcls.virtualNetworkRules) > 0
+| mvexpand vnr = properties.networkAcls.virtualNetworkRules
+| where vnr.id has_any (dynamic(["8453a5d5-9e9e-40c7-87a4-0ab4cc197f48",
+"31ef391b-7908-48ec-8c74-e432113b607b",
+"6c0d042c-6733-4420-a3cc-4175d0439b29",
+"23a8c420-c354-43f9-91f5-59d08c6b3dff",
+"9d5fffc7-7640-44a1-ba2b-f77ada7731d4",
+"56beece1-dbc8-40ca-8520-e1d514fb2ccc",
+"653c13e3-a85b-449b-9d14-e3e9c4b0d391",
+"b96a1dc5-559f-4249-a30c-5b5a98023c45",
+"b4f59749-ad17-4573-95ef-cc4c63a45bdf",
+"d31d7397-093d-4cc4-abd6-28b426c0c882",
+"d4a2f931-8db7-49ba-a9ea-18016369fbcb",
+"60baf026-ba3d-4d3d-a247-83c4e16439b7",
+"41a3b376-d525-4439-bd68-0d601a09e702",
+"6e628db8-cfec-4cd9-9b3c-789331b89103",
+"c10cf0cd-8008-4327-9402-46aa2337a1c9",
+"87ff4ec0-9212-4ce0-880d-1b479c031b8e"]))
+| project name, id, resourceGroup, subscriptionId, vnetRuleId = tostring(vnr.id), properties
+| summarize by id, name, vnetRuleId
+"@
 
+    $serviceEndpoints = Search-AzGraph -Query $sekql
+
+    foreach ($storageAccountObject in $serviceEndpoints) {
+        $vnetRuleId = $storageAccountObject.vnetRuleId
+        $separts = $vnetRuleId -split '/'
+        $subnetName = $separts[10]
+        $vnetName = $separts[8]
+        $vnetRGName = $separts[4]
+        Write-Host "Preparing to remove service endpoint for subnet '$subnetName' in VNet '$vnetName' in '$vnetRGName'..."
+        $parts = $storageAccountObject.id -split '/'
+        $resourceGroupName = $parts[4]
+        $storageAccountName = $parts[8]
+        Write-Host "Processing subnet rule ID: $vnetRuleId"
+        # Remove the virtual network rule from the storage account
+        Remove-AzStorageAccountNetworkRule -ResourceGroupName $resourceGroupName -Name $storageAccountName -VirtualNetworkResourceId $vnetRuleId
+        Write-Host "Successfully removed the service endpoint for subnet '$subnetName' in VNet '$vnetName' in '$vnetRGName' from storage account '$storageAccountName'."
+        
+    }
+}
 
 if ($interactive -eq $true) {
     Write-Host "Running in interactive mode. You will be prompted for each Storage Account association."
@@ -145,6 +201,7 @@ if (-not (Get-AzNetworkSecurityPerimeter -Name $nspName -ResourceGroupName $reso
 } 
 
 # Associate Storage Accounts with NSP
+
 foreach ($sa in $associateStorageAccount) {  
     if (Get-AzDenyAssignment -scope $sa.id -ErrorAction SilentlyContinue) {
         Write-Host "Skipping $($sa.name) : identified as workspace default storage (DBFS)."
@@ -172,12 +229,26 @@ foreach ($sa in $associateStorageAccount) {
         if ($userInput -eq 'Y') {
            Write-Host "Associating $($sa.name) with NSP $($nspName) in transition mode..."  
            New-AzNetworkSecurityPerimeterAssociation -Name "$($sa.name)-Assoc" -SecurityPerimeterName $nspName -ResourceGroupName $resourceGroup -ProfileId $nspProfile.Id -PrivateLinkResourceId $sa.id -AccessMode 'Learning' 
+           $SEdefaultValue = 'N'
+           $SEpromptMessage = "Remove Serverless Service Endpoints from $($sa.name) with NSP $($nspName) ? [Y/N, default: $SEdefaultValue]"
+           $SEresponse = Read-Host -Prompt $SEpromptMessage
+
+           # If the response is empty, use the default value. Otherwise, use the response.
+           $SEuserInput = if ([string]::IsNullOrEmpty($SEresponse)) { $SEdefaultValue } else { $SEresponse }
+           if ($SEuserInput -eq 'Y') {
+               Remove-Serverless-ServiceEndpoints -storageAccountName $sa.name
+           } else {
+               Write-Host "Skipping service endpoint removal for $($sa.name) as per user input."
+           }
         } else {
             Write-Host "Skipping $($sa.name) as per user input."
         }
     } else {
         Write-Host "Associating $($sa.name) with NSP $($nspName) in transition mode..."  
         New-AzNetworkSecurityPerimeterAssociation -Name "$($sa.name)-Assoc" -SecurityPerimeterName $nspName -ResourceGroupName $resourceGroup -ProfileId $nspProfile.Id -PrivateLinkResourceId $sa.id -AccessMode 'Learning'  
+        if ($Remove_Serverless_ServiceEndpoints -eq $true) {
+            Remove-Serverless-ServiceEndpoints -storageAccountName $sa.name
+        }
     }
 }
 
