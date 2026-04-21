90045c6f731da9e2df29fb59d793bc086e83ea21
Bruce Nelson <bruce.nelson@databricks.com>
2026-04-21 12:05:59 -0700

    added check for private endpoints

--- nsp-check.ps1
+++ nsp-check.ps1
@@ -49,53 +49,29 @@ function GetUserInput {
     $userInput = if ([string]::IsNullOrEmpty($response)) { $defaultValue } else { $response }
     return $userInput
 }
-# Define function to remove service endpoints
-function Remove-Serverless-ServiceEndpoints {
-param( [Parameter(Mandatory)]$storageAccountName)
-# Get the subnet object to retrieve its resource ID
-$sekql = @" 
+
+# Define a function to check for private endpoint connections
+function Check-PrivateEndpointConnections {
+    param( [Parameter(Mandatory)]$storageAccountName)
+    $pecsql = @"
 resources
 | where type == "microsoft.storage/storageaccounts"
 | where name == '$storageAccountName'
-| where array_length(properties.networkAcls.virtualNetworkRules) > 0
-| mvexpand vnr = properties.networkAcls.virtualNetworkRules
-| where vnr.id has_any (dynamic(["8453a5d5-9e9e-40c7-87a4-0ab4cc197f48",
-"31ef391b-7908-48ec-8c74-e432113b607b",
-"6c0d042c-6733-4420-a3cc-4175d0439b29",
-"23a8c420-c354-43f9-91f5-59d08c6b3dff",
-"9d5fffc7-7640-44a1-ba2b-f77ada7731d4",
-"56beece1-dbc8-40ca-8520-e1d514fb2ccc",
-"653c13e3-a85b-449b-9d14-e3e9c4b0d391",
-"b96a1dc5-559f-4249-a30c-5b5a98023c45",
-"b4f59749-ad17-4573-95ef-cc4c63a45bdf",
-"d31d7397-093d-4cc4-abd6-28b426c0c882",
-"d4a2f931-8db7-49ba-a9ea-18016369fbcb",
-"60baf026-ba3d-4d3d-a247-83c4e16439b7",
-"41a3b376-d525-4439-bd68-0d601a09e702",
-"6e628db8-cfec-4cd9-9b3c-789331b89103",
-"c10cf0cd-8008-4327-9402-46aa2337a1c9",
-"87ff4ec0-9212-4ce0-880d-1b479c031b8e"]))
-| project name, id, resourceGroup, subscriptionId, vnetRuleId = tostring(vnr.id), properties
-| summarize by id, name, vnetRuleId
+| where array_length(properties.privateEndpointConnections) > 0
+| mv-expand connections = properties.privateEndpointConnections
+| project 
+    StorageAccountName = name, 
+    ResourceGroup = resourceGroup, 
+    PrivateEndpointId = connections.properties.privateEndpoint.id,
+    Status = connections.properties.privateLinkServiceConnectionState.status,
+    GroupId = connections.properties.groupId
 "@
 
-    $serviceEndpoints = Search-AzGraph -Query $sekql
-
-    foreach ($storageAccountObject in $serviceEndpoints) {
-        $vnetRuleId = $storageAccountObject.vnetRuleId
-        $separts = $vnetRuleId -split '/'
-        $subnetName = $separts[10]
-        $vnetName = $separts[8]
-        $vnetRGName = $separts[4]
-        Write-Host "Preparing to remove service endpoint for subnet '$subnetName' in VNet '$vnetName' in '$vnetRGName'..."
-        $parts = $storageAccountObject.id -split '/'
-        $resourceGroupName = $parts[4]
-        $storageAccountName = $parts[8]
-        Write-Host "Processing subnet rule ID: $vnetRuleId"
-        # Remove the virtual network rule from the storage account
-        Remove-AzStorageAccountNetworkRule -ResourceGroupName $resourceGroupName -Name $storageAccountName -VirtualNetworkResourceId $vnetRuleId
-        Write-Host "Successfully removed the service endpoint for subnet '$subnetName' in VNet '$vnetName' in '$vnetRGName' from storage account '$storageAccountName'."
-        
+    $privateEndpoints = Search-AzGraph -Query $pecsql
+    if ($privateEndpoints -and $privateEndpoints.Count -gt 0) {    
+        return $true
+    } else {
+        return $false
     }
 }
 
@@ -208,6 +184,10 @@ if ($associateStorageAccount.Count -eq 0) {
         $parts = $sa.id -split '/'
         $resourceGroupName = $parts[4]
         Write-Host -ForegroundColor DarkCyan "`n- $($sa.name) Resource Group: $resourceGroupName Location: $($sa.location)"
+            # check for private endpoint connections and warn if found
+        if (Check-PrivateEndpointConnections -storageAccountName $sa.name) {
+            Write-Host -ForegroundColor Green "Warning: Storage Account '$($sa.name)' has private endpoint connections. Manual review of private endpoints and the resource firewall is strongly recommended."
+        }
     }
 
 }
